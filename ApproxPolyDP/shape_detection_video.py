67406bab65bb7124d1530a8a6431cce711302bab
=Batuhan Kerem <=batuhnck@gmail.com>
2022-12-24 13:05:40 +0300

    merkez belirleme, cv2 >cv, koortdinat tanımlama

--- ApproxPolyDP/shape_detection_video.py
+++ ApproxPolyDP/shape_detection_video.py
@@ -1,8 +1,12 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 # reading image
-cap = cv2.VideoCapture('Shapes Song.mp4')
+cap = cv.VideoCapture('Shapes Song.mp4')
+
+x = 127
+y = 127
+z = 127
 
 
 while (cap.isOpened()):
@@ -11,43 +15,64 @@ while (cap.isOpened()):
     if ret == True:
             
         # converting image into grayscale image
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         # setting threshold of gray image
-        _, threshold = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY)
-        dilation_threshold = cv2.dilate(threshold, (5,5), iterations=2)
+        _, threshold = cv.threshold(gray, 225, 255, cv.THRESH_BINARY)
+        dilation_threshold = cv.dilate(threshold, (5,5), iterations=2)
 
-        cv2.imshow('thresh', threshold)
-        cv2.waitKey(1)
+        cv.imshow('thresh', threshold)
+        cv.waitKey(1)
 
         # using a findContours() function
-        contours, _ = cv2.findContours(
-            threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv.findContours(
+            threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 
         # list for storing names of shapes
         for contour in contours:
 
             # cv2.approxPloyDP() function to approximate the shape
-            approx = cv2.approxPolyDP(
-                contour, 0.0175 * cv2.arcLength(contour, True), True)
+            approx = cv.approxPolyDP(
+                contour, 0.0175 * cv.arcLength(contour, True), True)
 
             # putting shape name at center of each shape
             if len(approx) == 4:
-                cv2.drawContours(frame, [contour], 0, (0, 255, 255), -1)
-                area = cv2.contourArea(contour)
+                cv.drawContours(frame, [contour], 0, (0, 255, 255), -1)
+                area = cv.contourArea(contour)
                 if area > 1000:
-                    cv2.drawContours(frame, [contour], 0, (255, 0, 0), -1)
-                    cv2.putText(frame, 'D', (contour[0][0][0], contour[0][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
+                    cv.drawContours(frame, [contour], 0, (255, 0, 0), -1)
+                    cv.putText(frame, 'D', (contour[0][0][0], contour[0][0][1]), cv.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
+                
+                M = cv.moments(contour)
+                if M['m00'] != 0:
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+                    cv.drawContours(frame, [i], -1, (0, 255, 0), 2)
+                    cv.circle(frame, (cx, cy), 7, (0, 0, 255), -1)
+                    cv.putText(frame, "center", (cx - 20, cy - 20),
+	        			cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+                print(f"x: {cx} y: {cy}")
 
             else:
-                area = cv2.contourArea(contour)
+                area = cv.contourArea(contour)
                 if area > 1000:
-                    cv2.drawContours(frame, [contour], 0, (255, 0, 0), -1)
-                    cv2.putText(frame, 'Y', (contour[0][0][0], contour[0][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
+                    cv.drawContours(frame, [contour], 0, (255, 0, 0), -1)
+                    cv.putText(frame, 'Y', (contour[0][0][0], contour[0][0][1]), cv.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
+
+                    M = cv.moments(contour)
+                    if M['m00'] != 0:
+                        cx = int(M['m10']/M['m00'])
+                        cy = int(M['m01']/M['m00'])
+                        cv.drawContours(frame, [i], -1, (0, 255, 0), 2)
+                        cv.circle(frame, (cx, cy), 7, (0, 0, 255), -1)
+                        cv.putText(frame, "center", (cx - 20, cy - 20),
+	                			cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+                print(f"x: {cx} y: {cy}")
+       
 
         # displaying the image after drawing contours
-        cv2.imshow('shapes', frame)
+        cv.imshow('shapes', frame)
 
-        cv2.waitKey(1)
+        cv.waitKey(1)
         #cv2.destroyAllWindows()
