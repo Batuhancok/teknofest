537d8420814f0813357e0bd973afe37b5db0db2f
=Batuhan Kerem <=batuhnck@gmail.com>
2023-01-27 23:18:55 +0300

    sınıf açıklamaları güncellendi

--- shape_decetion_2.py
+++ shape_decetion_2.py
@@ -12,10 +12,10 @@ class contour_finder():
         (cam_x, cam_y) = frame.shape[:2]
         cv.circle(frame, (cam_y // 2, cam_x // 2), 7, (255, 255, 255), -1)
             
-        # converting image into grayscale image
+        #her bir kareyi griye çevirdim, threshold uygulayabilmek için
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        # setting threshold of gray image
+        #Gri resme threshold ayarladım.
         _, threshold = cv.threshold(gray, 225, 255, cv.THRESH_BINARY)
         dilation_threshold = cv.dilate(threshold, (5,5), iterations=4)
         kernel = np.ones((5,5),np.uint8)
@@ -24,7 +24,7 @@ class contour_finder():
         cv.imshow('thresh', threshold)
         cv.waitKey(1)
 
-        # using a findContours() function
+        # kontürleri buldum
         contours, _ = cv.findContours(
             erosion, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         
@@ -38,16 +38,20 @@ class rectangle():
         self.contour = contour
 
     def rectangle_finder(self, frame, contour):
+
+        #kontürler arasında tek tek dolaşabildiğim döngü oluşturdum
         for contour in contour_finder.contour_calculator(frame):
-            # cv2.approxPloyDP() function to approximate the shape
+            
+            # nesne belirleme algoritması olan approxpolydp kullandım.
             approx = cv.approxPolyDP(
                 contour, 0.0175 * cv.arcLength(contour, True), True)
 
-            # putting shape name at center of each shape
+            # dikdörtgen tespiti yaptım
             if len(approx) == 4:
                 cv.drawContours(frame, [contour], 0, (0, 255, 255), -1)
                 area = cv.contourArea(contour)
 
+                #sadece ana şekli algılayabilmek için belirli bir alan sınırı koydum.
                 if (area > 1000 and area < 10000):
                     cv.drawContours(frame, [contour], 0, (255, 0, 0), -1)
                     cv.putText(frame, 'D', (contour[0][0][0], contour[0][0][1]), cv.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
@@ -68,7 +72,7 @@ class rectangle():
                     cv.putText(frame, "center", (cx - 20, cy - 20),
 	        			cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
                 
-                (cam_x, cam_y) = frame.shape[:2]
+                (cam_x, cam_y) = frame.shape[:2]#kameranın merkezini buldum.
                 while (area < 10000): #şekle aşırı yakın olmadığında şekli ortalamak için döngü. k = ufak dikdörtgenin bir kenarı
                     if(cam_x - cx < k/2):
                         x = 127
@@ -101,9 +105,11 @@ class circle():
         self.contour = contour
 
     def rectagle_detector(self, frame, contour):
-
+        
+        #kontürlerde gezebilmek için döngü oluşturdum.
         for contour in contour_finder.contour_calculator(frame):
-            # cv2.approxPloyDP() function to approximate the shape
+
+            # nesne belirleme algoritması olan approxpolydp kullandım.
             approx = cv.approxPolyDP(
                 contour, 0.0175 * cv.arcLength(contour, True), True)
 
