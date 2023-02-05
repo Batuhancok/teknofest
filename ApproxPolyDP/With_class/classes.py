445f7d2871948f92a7b436f72f8c6e7ba9bab885
=Batuhan Kerem <=batuhnck@gmail.com>
2023-02-05 19:54:17 +0300

    göndermeden önceki son hal

--- ApproxPolyDP/With_class/classes.py
+++ ApproxPolyDP/With_class/classes.py
@@ -8,6 +8,7 @@ class contour_finder():
     def __init__(self, frame):
         self.frame = frame
 
+    #contourleri bulup bunları çıktı veren bir fonksiyon.
     def contour_calculator(self, frame):
             
         #her bir kareyi griye çevirdim, threshold uygulayabilmek için
@@ -19,14 +20,11 @@ class contour_finder():
         kernel = np.ones((5,5),np.uint8)
         erosion = cv.erode(dilation_threshold,kernel,iterations = 4)
 
-        #cv.imshow('thresh', threshold)
-        #cv.waitKey(1)
-
         #İşlediğim görseldeki kontürleri buldum
         contours, _ = cv.findContours(
             erosion, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-        return contours
+        return contours #kontürleri çıktı olarak verdim
 
 
 class rectangle():
@@ -34,93 +32,95 @@ class rectangle():
     def __init__(self, frame):
         self.frame = frame
 
-
+    #dikdörtgen tespit algoritması.
     def rectangle_finder(self, frame):
-        for contour in contour_finder.contour_calculator(frame):
+        try:
+            for contour in contour_finder.contour_calculator(frame):
             
-            # nesne belirleme algoritması olan approxpolydp kullandım.
-            approx = cv.approxPolyDP(
-                contour, 0.0175 * cv.arcLength(contour, True), True)
+                # nesne belirleme algoritması olan approxpolydp kullandım.
+                approx = cv.approxPolyDP(
+                    contour, 0.0175 * cv.arcLength(contour, True), True)
 
-            # dikdörtgen tespiti yaptım
-            if len(approx) == 4:
-                #cv.drawContours(frame, [contour], 0, (0, 255, 255), -1)
-                return True
+                # dikdörtgen tespiti yaptım
+                if len(approx) == 4:
+                    return True
 
-            else:
-                return False
+                else:
+                    return False
 
+        except:#kontür bulamaması durumunda hata vermek yerine uyarı mesajı alacağız.
+            print("Dikdörtgen kontürleri bulunamadı.")
 
+    #dikdörtgen hareket algoritması
     def rectangle_move(self, frame):
-        
-        #try except ekle.
+        (cam_x, cam_y) = frame.shape[:2]#kameranın merkezini buldum.
+        contours = contour_finder.contour_calculator(frame)#kontürleri buldum.
 
-        #kontürler arasında tek tek dolaşabildiğim döngü oluşturdum
-        contours = contour_finder.contour_calculator(frame)
+        for contour in contours: #kontürler arasında gezecek döngü oluşturdum.
+            rotate_list = [0] * 4 #hareket işleminden sonra hareket komutu takılı kalmaması için sıfırladım.
+            area = cv.contourArea(contour)#şekle yakınlığı anlayabilmek için alan hesabı yaptım.
 
-        for contour in contours:
-
-            area = cv.contourArea(contour)
-            
-
-            #sadece ana şekli algılayabilmek için belirli bir alan sınırı koydum.
-            if (area > 1000 and area < 10000):
-                #cv.drawContours(frame, [contour], 0, (255, 0, 0), -1)
-                #cv.putText(frame, 'D', (contour[0][0][0], contour[0][0][1]), cv.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
-                #şeklin ortasını buldum ve isimlendirdim.
-
+            #Doğru şekli algılayabilmek için belirli bir alan sınırı koydum.
+            while (area > 1000 and area < 10000):
                 M = cv.moments(contour)
                 if M['m00'] != 0:
                     cx = int(M['m10']/M['m00'])
                     cy = int(M['m01']/M['m00'])
-                    #cv.drawContours(frame, [contour], -1, (0, 255, 0), 2)
-                    
-                    d_x,d_y,d_w,d_h = cv.boundingRect(contour)
-                    if(abs(d_x - d_w) > abs(d_y - d_h)):
-                        kisa_kenar = abs(d_y - d_h)
+                
+                #dikdörtgenin kısa kenar uzunluğuna eriştim.
+                d_x,d_y,d_w,d_h = cv.boundingRect(contour)
+                if(abs(d_x - d_w) > abs(d_y - d_h)):
+                    kisa_kenar = abs(d_y - d_h)
+                elif(abs(d_x - d_w) < abs(d_y - d_h)):
+                    kisa_kenar = abs(d_x - d_w)
 
-                    else:
-                        kisa_kenar = abs(d_x - d_w)
-
-                    #diktörgen şeklin ortasına şeklin o anki büyüklüğünün q katı kadar küçük yapay dikdörtgen çizdirdim.
-                    q = 3
-                    k = kisa_kenar/q
-                    a = (-1)*(kisa_kenar/(2*q))
-                    #cv.rectangle(frame, (cx + a, cy - a), (cx - a, cy + a), (0, 0, 255), 3)
-                    #cv.putText(frame, "center", (cx - 20, cy - 20),
-	            		#cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-            
-            (cam_x, cam_y) = frame.shape[:2]#kameranın merkezini buldum.
-            while (area < 10000): #şekle aşırı yakın olmadığında şekli ortalamak için döngü. k = ufak dikdörtgenin bir kenarı
-                rotate_list = [0] * 4
-
+                #diktörgenin ortasına şeklin o anki büyüklüğünün q katı kadar küçük yapay dikdörtgen oluşturdum.
+                q = 3
+                k = kisa_kenar/q #k = ufak dikdörtgenin kenarı
+                a = (-1)*(kisa_kenar/(2*q))        
+                
+                #yakınlaşmak için hız katsayısı
+                gain = 0.2
+                #Merkezin içinde değilse hareket komutu verilecek.
                 while(abs(cam_x - cx) > k/2):
+                    #sanal küçük dikdörtgenin ortası ile kamera ortasının kıyaslanması. X ekseni için
                     if(cam_x - cx < k/2):
-                        rotate_list[0] = 50
-                        return rotate_list
+                        #yaklaştıkça hızın yavaşlaması.
+                        rotate_list[0] = 50 + (gain * (cam_x - cx))
+                        #hız limiti.
+                        if (rotate_list[0] < 10):
+                            rotate_list[0] = 10
+                            return rotate_list
+                            
                     elif (cam_x - cx > k/2):
-                        rotate_list[0] = -50
+                        rotate_list[0] = -50 +(gain * (cam_x - cx))
+                        if (rotate_list[0] > -10):
+                            rotate_list[0] = -10
+                            return rotate_list
                         return rotate_list
                 
-                if(abs(cam_x - cx) < k/2):
-                    rotate_list[0] = 0
-                    return rotate_list
-                
+                #üstteki karşılaştırmanın y ekseni için yapılmış hali.
                 while(abs(cam_y - cy) > k/2):
                     if (cam_y - cy < k/2):
-                        rotate_list[2] = 50
-                        return rotate_list
-                    elif (cam_y - cy > k/2):
-                        rotate_list[2] = -50
+                        rotate_list[2] = 50 + (gain * (cam_x - cx))
+                        if (rotate_list[2] < 10):
+                            rotate_list[2] = 10
+                            return rotate_list
                         return rotate_list
 
-                if(abs(cam_y - cy) < k/2):
-                    rotate_list[2] = 0
-                    return rotate_list
+                    elif (cam_y - cy > k/2):
+                        rotate_list[2] = -50 + (gain * (cam_x - cx))
+                        if (rotate_list[2] > -10):
+                            rotate_list[2] = -10
+                            return rotate_list
+                        return rotate_list
+                
+                #kamera ortalandıysa araç yavaş yavaş ilerlesin.
+                rotate_list = [0, 15, 0, 0]
+                print(rotate_list)
             
             #kontürleri bütün olarak göremeyeceğim kadar yakınlaştığında aracın düz gitmesini belirttim.
-            while (area > 10000):
-                rotate_list = [0] * 4
+            if (area > 10000):
                 rotate_list[1] = 50
                 return rotate_list
 
@@ -130,29 +130,33 @@ class circle():
         self.frame = frame
         self.contour = contour
 
+    #daire bulma algoritması.
     def circle_finder(self, frame):
+        contours = contour_finder.contour_calculator(frame)
         
-        contours = contour_finder.contour_calculator(frame)
+        try:
+            #kontürlerde gezebilmek için döngü oluşturdum.
+            for contour in contours:
 
-        #kontürlerde gezebilmek için döngü oluşturdum.
-        for contour in contours:
+                # nesne belirleme algoritması olan approxpolydp kullandım.
+                approx = cv.approxPolyDP(
+                    contour, 0.0175 * cv.arcLength(contour, True), True)
 
-            # nesne belirleme algoritması olan approxpolydp kullandım.
-            approx = cv.approxPolyDP(
-                contour, 0.0175 * cv.arcLength(contour, True), True)
+                if len(approx) > 8: #daire ise True döndürecek koşul.
+                    return True
 
-            if len(approx) > 8: #daire ise True döndürecek koşul.
-                return True
-            
-            else:
-                return False
-                
+                else:
+                    return False
+        except:#kontür bulamaması durumunda hata vermek yerine uyarı mesajı alacağız.
+            print("Daire kontürleri bulunamadı.")
     
+    #daireye hareket algoritması.
     def circle_move(self, frame, contour):
-        (cam_x, cam_y) = frame.shape[:2]        
-        contours = contour_finder.contour_calculator(frame)
+        (cam_x, cam_y) = frame.shape[:2] #kameranın merkezini buldum.        
+        contours = contour_finder.contour_calculator(frame) #kontürleri buldum.
 
-        for contour in contours:
+        for contour in contours: #kontürler arasında gezecek döngü oluşturdum.
+            rotate_list = [0] * 4 #hareket işleminden sonra hareket komutu takılı kalmaması için sıfırladım.
             area = cv.contourArea(contour) 
 
             #ana şekli yüzde kaç oranında küçültüp merkeze çizeceğim yapay dairenin yarıçapı a. (değiştirilebilir.)
@@ -161,43 +165,57 @@ class circle():
             r = ((R**2)/a)**0.5 
             r = int(r)
 
-            #sadece ana şekli bulmak için alan filtreleme ve şekli isimlendirme
+            #sadece ana şekle odaklanabilmek için alan filtreleme ve şekli isimlendirme
             while (area > 1000 and area < 10000):
-                #cv.drawContours(frame, [contour], 0, (255, 0, 0), -1)
-                #cv.putText(frame, 'Y', (contour[0][0][0], contour[0][0][1]), cv.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
 
                 #merkeze daire çizme ve merkezi isimlendirme
                 M = cv.moments(contour)
                 if M['m00'] != 0:
                     cx = int(M['m10']/M['m00'])
                     cy = int(M['m01']/M['m00'])
-                    #cv.drawContours(frame, [contour], -1, (0, 255, 0), 2)
-                    #cv.circle(frame, (cx, cy), r, (0, 0, 255), 3) #içi boş ve daire boyutuyla orantılı bir çember çizdirdim. 
-                    #cv.putText(frame, "center", (cx - 20, cy - 20),
-	            			#cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
-
-                rotate_list = [0] * 4
-                
-                while(abs(cam_x - cx) > r): 
+                #yakınlaşmak için hız katsayısı
+                gain = 0.2
+                #Merkezin içinde değilse hareket komutu verilecek.
+                while(abs(cam_x - cx) > r):
+                    #sanal küçük dikdörtgenin ortası ile kamera ortasının kıyaslanması. X ekseni için
                     if(cam_x - cx < r):
-                        rotate_list[0] = 50
+                        #yakınlaştıkça hızı azalıyor.
+                        rotate_list[0] = 50 + (gain * (cam_x - cx))
+                        #hız limiti.
+                        if (rotate_list[0] < 10):
+                            rotate_list[0] = 10
+                            return rotate_list
                         return rotate_list
+
                     elif (cam_x - cx > r):
-                        rotate_list[0] = -50
+                        rotate_list[0] = -50 +(gain * (cam_x - cx))
+                        if (rotate_list[0] > -10):
+                            rotate_list[0] = -10
+                            return rotate_list
                         return rotate_list
-                
+
+                #üstteki karşılaştırmanın y ekseni için yapılmış hali.
                 while(abs(cam_y - cy) > r):
                     if (cam_y - cy < r):#yukarı çık
-                        rotate_list[2] = 50
+                        rotate_list[2] = 50 + (gain * (cam_x - cx))
+                        if (rotate_list[2] < 10):
+                            rotate_list[2] = 10
+                            return rotate_list
                         return rotate_list
                 
                     elif (cam_y - cy > r):#aşağı in
-                        rotate_list[2] = -50
+                        rotate_list[2] = -50 + (gain * (cam_x - cx))
+                        if (rotate_list[2] > -10):
+                            rotate_list[2] = -10
+                            return rotate_list
                         return rotate_list
 
-            #kontürleri bütün olarak göremeyeceğim kadar yakınlaştığında düz gitmesini belirttim.
+                #kamera ortalandıysa araç yavaş yavaş ilerlesin.
+                rotate_list = [0, 15, 0, 0]
+                print(rotate_list)
+
+            #kontürleri bütün olarak göremeyeceğim kadar yakınlaştığında aracın düz gitmesini belirttim.
             if (area > 10000):
-                rotate_list = [0] * 4
                 rotate_list[1] = 50
                 return rotate_list
--- ApproxPolyDP/With_class/runner.py
+++ ApproxPolyDP/With_class/runner.py
@@ -16,53 +16,66 @@ while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         
-        
+        #dikdörtgenden geçmediyse ve onu bulduysa içinden geçsin.
         if ((rectangle_pass == False) and (shape_detector.rectangle.rectangle_finder(frame) == True)):
+            
+            #dikdörtgen için bir hareket listesi oluşturan komut.
             rectangle_rotate_list = shape_detector.rectangle.rectangle_move(frame)
             
-            if (rectangle_rotate_list == [0, 50, 0, 0]):#eğer şekil geçebilecek yakınlıktaysa 7 saniye ileri gitsin ve dikdörtgen arama döngüsü kırılsın.
+            if (rectangle_rotate_list == [0, 50, 0, 0]):#eğer şekil geçebilecek yakınlıktaysa 7 saniye ileri gitsin ve dikdörtgen aaramyı bıraksın.
                 print(rectangle_rotate_list)
                 time.sleep(7)
                 rectangle_pass = True
                 
                 #180 derece dönme 
                 rectangle_rotate_list = [0, 0, 0, 20]
-                time.sleep(4)
                 print(rectangle_rotate_list)
-                break
+                time.sleep(4)
+
+                #döndükten sonra durma
+                rectangle_rotate_list = [0] * 4
+                print(rectangle_rotate_list)
 
             print(rectangle_rotate_list)
         
 
-        #Daire için hareket çıktı listesi oluşturuyorum.
+        #Küçük daireyi bulduysa ve onun içinden geçmediyse ona yönelsin.
         elif((small_circle_pass == False) and (shape_detector.circle.circle_finder(frame) == True)):
             contours = shape_detector.contour_finder.contour_calculator(frame)
             sorted_contours = sorted(contours, key=cv.contourArea, reverse=False) #birden fazla daire algılaması halinde küçükten büyüğe sıraladım.
             small_circle = sorted_contours[0]#en küçük daire.
 
+            #küçük daire için bir hareket listesi oluşturan komut.
             small_circle_rotate_list = shape_detector.circle.circle_move(frame, small_circle)
             
+            #geçecek kadar yakın komutu alıyorsak 7 saniye tüm komutları bırakıp ileri gitmeyi sürdürmeli.
             if (small_circle_rotate_list == [0, 50, 0, 0]):
                 print(small_circle_rotate_list)
                 time.sleep(7)
                 small_circle_pass = True
 
-                #180 derece dönme
+                #180 derece dönsün.
                 small_circle_rotate_list = [0, 0, 0, 20]
                 print(small_circle_rotate_list)
                 time.sleep(4)
-                break
+
+                #döndükten sonra durma.
+                small_circle_rotate_list = [0] * 4
+                print(small_circle_rotate_list)
 
             print(small_circle_rotate_list)
 
+        #büyük daireden geçmediyse ve onu bulduysa ona yönelsin.
         elif((big_circle_pass == False) and (shape_detector.circle.circle_finder(frame) == True)):
             
             contours = shape_detector.contour_finder.contour_calculator(frame)
             sorted_contours = sorted(contours, key=cv.contourArea, reverse=True) #birden fazla daire algılaması halinde büyükten küçüğe sıraladım.
             big_circle = sorted_contours[0]#büyük daire.
 
+            #büyük daire için bir hareket listesi oluşturan komut.
             big_circle_rotate_list = shape_detector.circle.circle_move(frame, big_circle)
             
+            #şekle çok yakında tüm komutları bırakıp 7 saniye ilerlesin.
             if (big_circle_rotate_list == [0, 50, 0, 0]):
                 print(big_circle_rotate_list)
                 time.sleep(7)
@@ -72,10 +85,14 @@ while (cap.isOpened()):
                 big_circle_rotate_list = [0, 0, 0, 20]
                 print(big_circle_rotate_list)
                 time.sleep(4)
-                break
+
+                #döndükten sonra durma.
+                big_circle_rotate_list = [0] * 4
+                print(big_circle_rotate_list)
 
             print(big_circle_rotate_list)
         
+        #tüm şekillerden geçtiyse dursun. Ama araç şekil algılayamasa da duruyor?
         else:
             rotate_list = [0] * 4
             print (rotate_list)
